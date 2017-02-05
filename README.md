b13b9b9c24ce00cf02211a84a8841da1593e0182
minbr0ther <minbr0ther@naver.com>
2017-02-05 18:55:48 +0900

    second

---
+++ README.md
@@ -0,0 +1,32 @@
+# project myBlog
+> 매일매일 회고 형식으로 일지를 작성한다
+
+
+## 17.01.31
+* https://maaemo.no/ 
+* 위 사이트를 기반으로 내 블로그를 만들어 본다
+
+* [backlog](https://docs.google.com/spreadsheets/d/1U64eg0LnYGroW6nOHQUJzeRg6Tkr1Q8bW6HEZP0Vx0A/edit?usp=sharing)작성
+
+
+## 17.02.01
+* 홈페이지 마크업 절반정도 완성
+
+
+## 17.02.02
+* 홈페이지 마크업 완성
+* :hover 글씨회색
+* mouseOnOver 그림 밝기 조절 (미완성)
+
+
+## 17.02.03
+* [left-push-menu](https://codepen.io/anon/pen/MJGOKL) 제작 
+* 메뉴 [toggle](http://jsfiddle.net/fyUJc/40/)버튼 구현 
+
+
+## 17.02.04
+* 스크롤시 메뉴 사라지는 기능 구현
+
+
+## 17.02.05
+* hire me 기능 구현중
---
+++ project_MyBlog.js
@@ -0,0 +1,46 @@
+// right menu push in, out
+var menu = document.querySelector("#menu");
+var menuBtn = document.querySelector("#menuBtn");
+var frontPage = document.querySelector(".frontPage");
+var hireMe = document.querySelector(".hireMe");
+var hireMeBtn = document.querySelector("#hireMeBtn");
+var pushDown = document.querySelector(".pushDown");
+
+
+menuBtn.addEventListener("click", function(){
+	if(menu.style.transform == "translateX(-450px)"){ 
+	//메뉴가 보인다면 집어 넣기 
+		menu.style.transform = "translateX(450px)";
+		frontPage.style.transform = "translateX(0px)";
+	}
+	else { //메뉴가 안보인다면 튀어나오기 
+		menu.style.transform = "translateX(-450px)";
+		frontPage.style.transform = "translateX(-200px)";		
+	}
+});
+
+window.onscroll = function(){ //메뉴창이 나와있을때 스크롤 하면 사라진다
+	if(menu.style.transform == "translateX(-450px)"){
+		menu.style.transform = "translateX(450px)";
+		frontPage.style.transform = "translateX(0px)";
+	}
+};
+
+function textTranslation(){ //구현중 170205
+	hireMeBtn.style = "transform:translateY(-10px); opacity:0;" 
+}
+
+hireMeBtn.addEventListener("click", function(){
+	if(hireMe.style.transform == "translateY(700px)"){
+		//hire me가 보인다면 집어 넣기 
+		pushDown.style.transform = "translateY(0px)";
+		setTimeout(function(){
+			hireMe.style.transform = "translateY(-700px)";
+		}, 490);
+	}
+	else { //hire me가 안보인다면 튀어나오기
+		// textTranslation();
+		hireMe.style.transform = "translateY(700px)";
+		pushDown.style.transform = "translateY(700px)";
+	}
+});

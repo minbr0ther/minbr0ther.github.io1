e469d09b995833c44b9bd3b9986bf2ef112b7fd0
minbr0ther <minbr0ther@naver.com>
2017-02-06 22:21:25 +0900

    4

--- project_MyBlog.js
+++ project_MyBlog.js
@@ -5,6 +5,7 @@ var frontPage = document.querySelector(".frontPage");
 var hireMe = document.querySelector(".hireMe");
 var hireMeBtn = document.querySelector("#hireMeBtn");
 var pushDown = document.querySelector(".pushDown");
+var toTopBtn = document.querySelector(".toTopBtn");
 
 
 menuBtn.addEventListener("click", function(){
@@ -19,11 +20,22 @@ menuBtn.addEventListener("click", function(){
 	}
 });
 
-window.onscroll = function(){ //메뉴창이 나와있을때 스크롤 하면 사라진다
+window.onscroll = function(){ 
 	if(menu.style.transform == "translateX(-450px)"){
+		//메뉴창이 나와있을때 스크롤 하면 사라진다
 		menu.style.transform = "translateX(450px)";
 		frontPage.style.transform = "translateX(0px)";
 	}
+	else if(hireMe.style.transform == "translateY(700px)"){
+		//hire me가 보인다면 집어 넣기 
+		pushDown.style.transform = "translateY(0px)";
+		setTimeout(function(){
+			hireMe.style.transform = "translateY(-700px)";
+		}, 100);
+	}
+	else if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        toTopBtn.style.transform = "translateY(-122px)";
+    }
 };
 
 function textTranslation(){ //구현중 170205
@@ -43,4 +55,19 @@ hireMeBtn.addEventListener("click", function(){
 		hireMe.style.transform = "translateY(700px)";
 		pushDown.style.transform = "translateY(700px)";
 	}
+});
+
+// window.onscroll = function(ev) {
+//     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+//         toTopBtn.style.transform = "translateY(-122px)";
+//     }
+// };
+
+toTopBtn.addEventListener("click", function(){
+	pushDown.scrollIntoView({ behavior: "smooth"});
+	// window.scrollTo({
+ //    "behavior": "smooth",
+ //    "left": left,
+ //    "top": top
+	// });
 });
